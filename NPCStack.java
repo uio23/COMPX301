35b2f0d3eaefa458d0197399008373bbd8b96010
Alexander <ionchannel@runbox.com>
2025-06-05 15:29:48 +0000

    Tidy implementation code

--- NPCStack.java
+++ NPCStack.java
@@ -2,55 +2,90 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class NPCStack {
-	private static ArrayList<Box> boxes = new ArrayList<Box>();
-	private static int[] bitmap;
-
 	public static void main(String[] args) {
+		// Verify the valid number of command line arguments have been passed
 		if (args.length != 3) {
-			System.err.println("Usage: java NPCStack filename initial_temp cooling_rate");
+			System.err.println("Usage: java NPCStack filename initialTemp coolingRate");
 			System.exit(1);
 		}
 
 		String filename = args[0];
-		loadBoxes(filename);
-		generateInitialSubsequence();
-		int sum = 0;
-		for(int i = 0; i < bitmap.length; i++) {
-			if (bitmap[i] == 1) {
-				sum += boxes.get(i).h;
-			}
-		}
-		System.out.println("Initial solution: " + sum);
 
-		int temp = Integer.parseInt(args[1]);
-		float cooling = Float.parseFloat(args[2]);
-		annealing((float)temp, cooling);
-		sum = 0;
-		for(int i = 0; i < bitmap.length; i++) {
-			if (bitmap[i] == 1) {
-				sum += boxes.get(i).h;
-			}
+		// Try parse the numerical command line arguments, and exit the program if that fails
+		int initialTemp = -1;
+		double coolingRate = -1;
+		try {
+			initialTemp = Integer.parseInt(args[1]);
 		}
-		System.out.println("Annealing solution: " + sum);
+		catch (NumberFormatException e) {
+			System.err.println("Could not parse command line argument, initialTemp must be an integer");
+			System.exit(1);
+		}
+		try {
+			coolingRate = Double.parseDouble(args[2]);
+		}
+		catch (NumberFormatException e) {
+			System.err.println("Could not parse command line argument, coolingRate must be a real number");
+			System.exit(1);
+		}
+
+		Box[] boxes = loadBoxes(filename);
+		Arrays.sort(boxes);
+
+		// Ensure that the annealing parameters are withing their limits
+		if (initialTemp <= 0 || initialTemp > boxes.length) {
+			System.err.println("initialTemp must be 0 < initialTemp <= 3*(N of boxes)");
+			System.exit(1);
+		}
+		if (coolingRate < 0.1 || coolingRate > initialTemp) {
+			System.err.println("coolingRate must be 0.1 <= coolingRate <=t");
+			System.exit(1);
+		}
+
+		int[] bitmap = solveWithDP(boxes);
+		displayBitmap(bitmap);
+		display(bitmap, boxes);
+		BitmapManager.removeDuplicateBoxes(bitmap, boxes);
+		displayBitmap(bitmap);
+		display(bitmap, boxes);
+		annealing(bitmap, boxes, initialTemp, coolingRate);
+		displayBitmap(bitmap);
+		display(bitmap, boxes);
+	}
+
+	private static void displayBitmap(int[] bitmap) {
+		for(int bit : bitmap) {
+			System.out.print(bit);
+		}
+		System.out.println();
 	}
 	
-	static private void display() {
+	static private void display(int[] bitmap, Box[] boxes) {
 		Box box;
 		int height = 0;
 
-		for (int i = 0; i < bitmap.length; i++) {
+		// Calculate the height of the bitmap stack
+		for (int i = 0; i < bitmap.length;  i++) {
 			if (bitmap[i] == 1) {
-				box = boxes.get(i);
-				height += box.h;
+				height += boxes[i].h;
+			}
+		}
+
+		// Display every box in the bitmap from the highest one down, alongside the high at that level
+		for (int i = bitmap.length - 1; i >= 0;  i++) {
+			if (bitmap[i] == 1) {
+				box = boxes[i];
 				System.out.format("%d %d %d %d\n", box.w, box.l, box.h, height);
+				height -= box.h;
 			}
 		}
 	}
 
-	private static void annealing(float temp, float cooling) {
+	private static void annealing(int[] bitmap, Box[] boxes, int temp, double cooling) {
 		int changesN;
 		int changedBit;
 		Random random = new Random();
@@ -90,176 +125,121 @@ public class NPCStack {
 		}
 	}
 
-	private static void loadBoxes(String filename) {
+	private static Box[] loadBoxes(String filename) {
 		String line;
-		String[] words;
-		int h, l, w;
-		int boxN = 0;
+		String[] lineWords;
+		int w, l, h;
+
+		ArrayList<Box> boxes = new ArrayList<>();
 
 		try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+			// Create 3 boxes for every line in the file, skipping any invalid lines
 			while((line = reader.readLine()) != null) {
-				words = line.split(" ");
+				lineWords = line.split(" ");
 
-				if (words.length != 3) {
+				// Verify that there are 3 values on this line
+				if (lineWords.length != 3) {
 					continue;
 				}
 
-				h = l = w = -1;
+				// Convert the 3 values to integers and verify they are all positive
 				try {
-					h = Integer.valueOf(words[2]);
-					l = Integer.valueOf(words[1]);
-					w = Integer.valueOf(words[0]);
+					h = Integer.valueOf(lineWords[2]);
+					l = Integer.valueOf(lineWords[1]);
+					w = Integer.valueOf(lineWords[0]);
 				}
 				catch (NumberFormatException e) {
 					continue;
 				}
-
 				if (h < 0 || l < 0 || w < 0) {
 					continue;
 				}
 
-				boxes.add(new Box(h, l, w, boxN));
-				if (h != l) {
-					boxes.add(new Box(l, h, w, boxN));
-				}
-				if (h != w) {
-					boxes.add(new Box(w, l, h, boxN));
-				}
-				boxN++;
+				// Create a box for each of the 3 possible heights of this box
+				boxes.add(new Box(w, l, h));
+				boxes.add(new Box(h, w, l));
+				boxes.add(new Box(l, h, w));
 			}
 		}
 		catch (IOException error) {
-			String errorMess = String.format("Error reading from file: %s", error.getMessage());
-			System.err.println(errorMess);
+			// Exit the program with an error if the file could not be read completely/successfully
+			System.err.printf("Error reading from file: %s\n", error.getMessage());
 			System.exit(1);
 		}
-		bitmap = new int[boxes.size()];
+
+
+		// Return an array of the created box objects
+		return boxes.toArray(new Box[0]);
 	}
 
-	private static void solveLDS() {
-		boxes.sort(null);
-		int[] maxH = new int[boxes.size()];
-		int[] nextBox = new int[boxes.size()];
+	private static int[] solveWithDP(Box[] boxes) {
+		// For every box, stores the maximum stack heigh of the box, i.e. where this box is at the top
+		int[] H = new int[boxes.length];
+		// For every box, store the index of the previous box in the tallest stack of this box
+		int[] boxBelowI = new int[boxes.length];
+
+		int maxH = 0;
+		int bestStackIndex = 0;
+
 		Box currentBox, boxBelow;
 
-		for (int i = 0; i < maxH.length; i++) {
-			currentBox = boxes.get(i);
-			maxH[i] = currentBox.h;
+		for (int i = 0; i < boxes.length; i++) {
+			currentBox = boxes[i];
+			// Initialise the maximum height of currentBox's stack to be the height of currentBox
+			H[i] = currentBox.h;
+
+			// Test putting this box on every box below this one, which may be the top of its own stack
 			for (int j = 0; j < i; j++) {
-				boxBelow = boxes.get(j);
+				boxBelow = boxes[j];
 
+				// Verify contraint for putting currentBox on top of this boxBelow
 				if (currentBox.l < boxBelow.l && currentBox.w < boxBelow.w) {
-					if (maxH[j] + currentBox.h > maxH[i]) {
-						maxH[i] = maxH[j] + currentBox.h;
-						nextBox[i] = j;
+					// If this also increases the high of the stack where currentBox is at the top
+					if (H[j] + currentBox.h > H[i]) {
+						// Update the maximum height of the stack with currentBox at the top
+						H[i] = H[j] + currentBox.h;
+
+						// Record that currentBox is going on top of this boxBelow
+						boxBelowI[i] = j;
 					}
+				}
+				
+				// If the height achieved by currentBox's stack is the tallest so far,
+				// record the idex of currentBox as the start of the tallest stack
+				if (H[i] > maxH) {
+					maxH = H[i];
+					bestStackIndex = i;
 				}
 			}
 		}
-		
-		getBitmapOfLDS(nextBox, maxH);
-	}
 
-	private static void getBitmapOfLDS(int[] nextBox, int[] H) {
-		int maxHeightI = 0;
-		for (int i = 0; i < nextBox.length; i++) {
-			if(H[i] > H[maxHeightI]) {
-				maxHeightI = i;
-			}
-		}
-		System.out.println("DP solution: " + H[maxHeightI]);
-		int currI = maxHeightI;
+		// Convert the solution to be a bitmap of the boxes included
+		// by following the indicies in boxBelow for the best stack
+		int[] bitmap = new int[boxes.length];
 		do {
-			bitmap[currI] = 1;
-			currI = nextBox[currI];
+			bitmap[bestStackIndex] = 1;
+			bestStackIndex = boxBelowI[bestStackIndex];
 		}
-		while(currI > 0);
+		while(bestStackIndex > 0);
+
+		return bitmap;
 	}
-	
-	public static void generateInitialSubsequence() {
-		solveLDS();
-		removeDuplicateBoxUse();
-	}
-
-	private static void removeDuplicateBoxUse() {
-		for (int boxI = 0; boxI < bitmap.length + 2; boxI+=3) {
-			int maxH = 0;
-			int maxHI = -1;
-			for (int rotationI = 0; rotationI < 3; rotationI++) {
-				if (boxI+rotationI >= bitmap.length) {
-					break;
-				}
-				if (bitmap[boxI+rotationI] == 1 && boxes.get(boxI+rotationI).h > maxH) {
-					maxH = boxes.get(boxI+rotationI).h;
-					maxHI = boxI + rotationI;
-				}
-				bitmap[boxI+rotationI] = 0;
-			}
-			if (maxHI > 0) {
-				bitmap[maxHI] = 1;
-			}
-		}
-	}
-
-	private static int evaluateBitmap(int[] mapToUse) {
-		int score = 0;
-		for (int i = 0; i < mapToUse.length; i++) {
-			if (mapToUse[i] == 1) {
-				score += boxes.get(i).h;
-			}
-		}
-
-		return score;
-	}
-
-	private static boolean validateBitmap(int[] mapToUse) {
-		int rotation;
-		int boxSum = 0;
-		int prev = -1;
-
-		for (int i = 0; i < mapToUse.length; i++) {
-			rotation = i % 3;
-			if (rotation == 0) {
-				if (boxSum > 1) {
-					return false;
-				}
-				boxSum = 0;
-			}
-			// If this box violates the contraint
-			if (mapToUse[i] == 1) {
-				if (prev >= 0) {
-					if (boxes.get(prev).w <= boxes.get(i).w || boxes.get(prev).l <= boxes.get(i).l) {
-						return false;
-					}
-				}
-
-				boxSum++;
-				prev = i;
-			}
-		}
-		if (boxSum > 1) {
-			return false;
-		}
-
-		return true;
-	}
-
 }
 
-class Box implements Comparable<Box> {
-	public int h, l, w, id;
 
-	public Box(int h, int l1, int l2, int id) {
-		this.h = h;
-		if (l1 > l2) {
-			this.w = l1;
-			this.l = l2;
+class Box implements Comparable<Box> {
+	public int h, l, w;
+
+	public Box(int x, int y, int z) {
+		if (x > y) {
+			w = x;
+			l = y;
 		}
 		else {
-			this.l = l1;
-			this.w = l2;
+			w = y;
+			l = x;
 		}
-		this.id = id;
+		h = z;
 	}
 
 	/**
@@ -267,5 +247,98 @@ class Box implements Comparable<Box> {
 	 */
 	public int compareTo(Box box) {
       return box.w * box.l - w * l;
+	}
+}
+
+class BitmapManager {
+	public static void removeDuplicateBoxes(int[] bitmap, Box[] boxes) {
+		int i;
+		int maxHeightRotation;
+		int heighestRotationI;
+
+		// For every box in the bitmap
+		for (int boxI = 0; boxI < bitmap.length; boxI+=3) {
+			maxHeightRotation = -1;
+			heighestRotationI = -1;
+
+			// For every rotation of a box
+			for (int rotationI = 0; rotationI < 3; rotationI++) {
+				// Calc index of this box's current rotation
+				i = boxI+rotationI;
+
+				// If this rotation is present in the bitmap and is the taller one...
+				if (bitmap[i] == 1 && boxes[i].h > maxHeightRotation) {
+					// Record this rotation as the tallest one
+					maxHeightRotation = boxes[i].h;
+					heighestRotationI = boxI + rotationI;
+				}
+
+				// Remove the rotation from the bitmap
+				bitmap[i] = 0;
+			}
+
+			// Restore the heighest rotation of this box in the bitmap, if it was present
+			if (heighestRotationI > 0) {
+				bitmap[heighestRotationI] = 1;
+			}
+		}
+	}
+
+	public static boolean validateBitmap(int[] bitmap, Box[] boxes) {
+		int rotationI;
+		int rotationsSum = 0;
+		int prev = -1;
+
+		for (int i = 0; i < bitmap.length; i++) {
+			rotationI = i % 3;
+
+			// If this is the first rotation of a box, check that at most one rotation of
+			// the previous box was included in the bitmap
+			if (rotationI == 0) {
+				// If more than one rotation of a box is included in the bitmap,
+				// this bitmap contains a repeating box and is invalid
+				if (rotationsSum > 1) {
+					return false;
+				}
+				// Reset the rotation sum for the new box
+				rotationsSum = 0;
+			}
+
+			// If this box rotation is included
+			if (bitmap[i] == 1) {
+				// If the box below this one exists and has a width or length less than or equal to that of the current box,
+				// the stacking constrain is violated and this bitmap is invalid
+				if (prev >= 0) {
+					if (boxes[prev].w <= boxes[i].w || boxes[prev].l <= boxes[i].l) {
+						return false;
+					}
+				}
+
+				// Account for the inclusion of this rotation
+				rotationsSum++;
+				// Record this box as the previous box for the box above
+				prev = i;
+			}
+		}
+		// Verify the last box was only included in one rotation
+		if (rotationsSum > 1) {
+			return false;
+		}
+
+		// If no contraints are violated, this bitmap is valid
+		return true;
+	}
+
+	public static int envaluateBitmap(int[] bitmap, Box[] boxes) {
+		int score = 0;
+
+		// Every included box contributes its height to the score
+		for (int i = 0; i < bitmap.length; i++) {
+			if (bitmap[i] == 1) {
+				score += boxes[i].h;
+			}
+		}
+
+		return score;
 	}
 }
